c13f55cba0322155620fafdc26511cd8a74ec5f6
kiss myass <ak29081708@yahoo.com.tw>
2020-07-07 17:47:42 +0800

    Add code

--- app.py
+++ app.py
@@ -38,22 +38,19 @@ what_can_i_do="我目前只有2個按鈕\n然後我可以學你說話\n你也可
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text=="@自我介紹":
-        message = TextSendMessage(text="哈摟我叫yichengBOT 建於2020/7/7 沒啥功能")
+        message = TextSendMessage(text="哈摟我叫yichengBOT\n建於2020/7/7\n沒啥功能")
         line_bot_api.reply_message(event.reply_token, message)
     elif event.message.text=="@你會幹嘛?":
         message = TextSendMessage(text=what_can_i_do)
         line_bot_api.reply_message(event.reply_token, message)
     elif event.message.text=="@風景圖":
-        beach_picture()
+        message = ImageSendMessage(
+            original_content_url='https://images4.alphacoders.com/774/77454.jpg',
+            preview_image_url='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png')
+        line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
-
-def beach_picture():
-    message = ImageSendMessage(
-            original_content_url='https://images4.alphacoders.com/774/77454.jpg',
-            preview_image_url='https://images4.alphacoders.com/774/77454.jpg')
-    line_bot_api.reply_message(event.reply_token, message)
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
