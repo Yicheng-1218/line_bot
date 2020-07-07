520a0c6d7421b925025ade63af3592c16a55cd6e
kiss myass <ak29081708@yahoo.com.tw>
2020-07-07 15:44:00 +0800

    Add code

--- app.py
+++ app.py
@@ -29,7 +29,11 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-what_can_i_do="我目前只有2個按鈕，然後我可以學你說話"
+
+what_can_i_do="我目前只有2個按鈕，然後我可以學你說話 你也可以試試輸入  @風景圖"
+
+
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -42,7 +46,12 @@ def handle_message(event):
     else:
         message = TextSendMessage(text=event.message.text)
         line_bot_api.reply_message(event.reply_token, message)
-
+def beach_picture(event):
+    if event.message.text=="@風景圖":
+        message = ImageSendMessage(
+            original_content_url='https://images4.alphacoders.com/774/77454.jpg',
+            preview_image_url='https://images4.alphacoders.com/774/77454.jpg')
+        line_bot_api.reply_message(event.reply_token, message)
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
