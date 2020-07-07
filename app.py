a0cada373d3ff5519374719ffce026876d98f95b
kiss myass <ak29081708@yahoo.com.tw>
2020-07-07 15:11:52 +0800

    Add code

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = event.message.text
     if message=="@自我介紹":
         Self_introduction="哈摟我是yichengBOT 因為剛建起來，所以我超笨"
         line_bot_api.reply_message(event.reply_token, Self_introduction)
