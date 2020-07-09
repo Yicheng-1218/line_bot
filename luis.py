53391f8dc085672281535c14023458587168c5a2
kiss myass <ak29081708@yahoo.com.tw>
2020-07-09 22:08:09 +0800

    Add code

--- luis.py
+++ luis.py
@@ -18,17 +18,14 @@ params ={
 }
 def luis_read(user_input):
     try:
-        query=" "
-        while True :
-            query = user_input
-            if(query=="") :
-                break
-            params['q']=query
-            r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/445b8090-2457-4d6a-b2d5-7f88b85b5806',headers=headers, params=params)
-            # 顯示結果
-            ret=r.json()
-            user_intent=ret['topScoringIntent']
-            for entity in ret['entities'] :
-                user_entities.append(entity)
+        query = user_input
+        params['q']=query
+        r = requests.get('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/445b8090-2457-4d6a-b2d5-7f88b85b5806',headers=headers, params=params)
+        # 顯示結果
+        ret=r.json()
+        user_intent=ret['topScoringIntent']
+        for entity in ret['entities'] :
+            user_entities.append(entity)
     except Exception as e:
         print(e)
+
