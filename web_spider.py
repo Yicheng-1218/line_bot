a8727ab852cdaf75fa94cb0e908064ddad2b4aa1
kiss myass <ak29081708@yahoo.com.tw>
2020-07-09 18:23:45 +0800

    Add code

--- web_spider.py
+++ web_spider.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import requests
 
 def WeatherGet(user_location):
@@ -39,3 +40,24 @@ def WeatherGet(user_location):
         show='無此縣市資料'
 
     return show
+
+src=[]
+
+def memesPage():
+    index=0
+    if not src:
+        index+1
+        Meme_New_List()
+    return index
+
+def Meme_New_List():
+    meme_html=requests.get('https://memes.tw/wtf?sort=top-month&contest=11&page='+memesPage())
+    soup = BeautifulSoup(meme_html.text, 'html.parser')
+    img_links = soup.find_all('img','img-fluid lazy')
+    for imgs in img_links:
+        src.append(imgs.get('data-src'))
+    memesPage()
+
+def MemeSend():
+    memesPage()
+    return src.pop()
